65249bd294dcce79f8220c900ac289b8541e482d
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-01-24 11:31:00 -0500

    [add] Added Redux, Reducers, Thunk/Logger Middlewear

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware} from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import "./index.css";
+
+import { smurfReducer } from './reducers/index';
+
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(smurfReducer, applyMiddleware(thunk, logger));
+console.log(store.getState());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById("root"));
---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,13 @@
+export const initiialState = {
+    data: []
+}
+
+
+export const smurfReducer = (state = initiialState, action) => {
+
+    switch(action.type){
+        default:
+            return state;
+    }
+
+};
